cfffe8b130515edd67ab874e6bfd19039af273e0
Deiker Vasquez <deiker21004@gmail.com>
2024-05-08 14:47:07 -0400

    home rut

--- src/components/content-main/header.jsx
+++ src/components/content-main/header.jsx
@@ -4,7 +4,7 @@ export default function Header({name}){
     const refButton = useRef(null);
     return(
         <div className='headerChocolate'>
-            <div className='container_icon'><a href='https://www.shekoh.com/flavors'><img className='iconHeader' src={icon} alt="" /></a></div>
+            <div className='container_icon'><a href='https://www.shekoh.com/experience'><img className='iconHeader' src={icon} alt="" /></a></div>
             <div>
             <h1 className='chocolate_name'>{name}</h1>
             </div>
